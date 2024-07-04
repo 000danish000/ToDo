96ca4df2258e1e87403d1337bcac7ceedc399a1c
000danish000 <dhpinjari@outlook.com>
2024-07-04 18:19:37 +0530

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -5,11 +5,19 @@ const deleteItem = document.querySelectorAll("li i .fa .fa-times")
 userInput.addEventListener("keyup", (e)=>{
     if (e.code === "Enter" || e.code === "NumpadEnter") {
         let newListItem = document.createElement("li");
-        newListItem.textContent = userInput.value;
+        newListItem.innerHTML = `${userInput.value} <i class="fa fa-times"></i>`;
         taskList.appendChild(newListItem);
         newListItem.addEventListener("click", () => {
             newListItem.classList.toggle("completed");
           });
+
+
         userInput.value='';
+        
+        newListItem.querySelector("i").addEventListener("click",()=>{
+            newListItem.remove();
+        })
+
+
     }
 })
