6d72d9cadbad4452a9bc20a26f598a952b95a43f
000danish000 <dhpinjari@outlook.com>
2024-07-05 12:39:30 +0530

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -1,23 +1,37 @@
 const userInput = document.getElementById('userInput');
 const taskList = document.getElementById('task');
+const addTaskBtn = document.querySelector(".addTaskBtn");
 const deleteItem = document.querySelectorAll("li i .fa .fa-times")
+
+
+
+const createTask = () =>{
+
+    let newListItem = document.createElement("li");
+    newListItem.innerHTML = `${userInput.value} <i class="fa fa-times"></i>`;
+    taskList.appendChild(newListItem);
+    newListItem.addEventListener("click", () => {
+        newListItem.classList.toggle("completed");
+      });
+    
+    
+    userInput.value='';
+    
+    newListItem.querySelector("i").addEventListener("click",()=>{
+        newListItem.remove();
+    })  
+    }
 
 userInput.addEventListener("keyup", (e)=>{
     if (e.code === "Enter" || e.code === "NumpadEnter") {
-        let newListItem = document.createElement("li");
-        newListItem.innerHTML = `${userInput.value} <i class="fa fa-times"></i>`;
-        taskList.appendChild(newListItem);
-        newListItem.addEventListener("click", () => {
-            newListItem.classList.toggle("completed");
-          });
-
-
-        userInput.value='';
-        
-        newListItem.querySelector("i").addEventListener("click",()=>{
-            newListItem.remove();
-        })
-
+        createTask();
 
     }
 })
+
+
+addTaskBtn.addEventListener("click",()=>{
+    createTask();
+})
+
+
