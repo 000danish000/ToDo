9fd37d9b4ca5efc7bfe1d50db77b9a1f900ca922
000danish000 <dhpinjari@outlook.com>
2024-07-26 17:54:47 +0530

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -1,37 +1,72 @@
 const userInput = document.getElementById('userInput');
 const taskList = document.getElementById('task');
 const addTaskBtn = document.querySelector(".addTaskBtn");
-const deleteItem = document.querySelectorAll("li i .fa .fa-times")
+const deleteItem = document.querySelectorAll("li i .fa .fa-times");
+const errMsg = document.getElementById('msg')
 
 
+function saveTask() {
+    const lsTask = document.querySelectorAll('#task li');
+    const data = [];
+    lsTask.forEach((ls) => {
+        data.push(ls.textContent);
+    });
+    localStorage.setItem("tasks", JSON.stringify(data));
 
-const createTask = () =>{
-
+}
+const createTask = (taskValue) => {
     let newListItem = document.createElement("li");
-    newListItem.innerHTML = `${userInput.value} <i class="fa fa-times"></i>`;
+    newListItem.innerHTML = `${taskValue}<span class="icon"><i class="fa-regular fa-pen-to-square"></i><i class="fa-solid fa-xmark"></i></span>`;
     taskList.appendChild(newListItem);
     newListItem.addEventListener("click", () => {
         newListItem.classList.toggle("completed");
-      });
-    
-    
-    userInput.value='';
-    
-    newListItem.querySelector("i").addEventListener("click",()=>{
+    });
+    userInput.value = '';
+    newListItem.querySelector(".fa-xmark").addEventListener("click", () => {
         newListItem.remove();
-    })  
-    }
+        saveTask();
+    })
+    newListItem.querySelector(".fa-pen-to-square").addEventListener("click", () => {
+        if (userInput.value == "") {
+            userInput.value = newListItem.textContent;
+            newListItem.remove();
+        } else {
+            errMsg.style.display = "block";
+            errMsg.innerHTML = "Save the Existing Task First";
+            setTimeout(() => {
+                errMsg.style.display = "none";
+            }, 3000);
+        }
 
-userInput.addEventListener("keyup", (e)=>{
+
+    })
+
+}
+
+userInput.addEventListener("keyup", (e) => {
     if (e.code === "Enter" || e.code === "NumpadEnter") {
-        createTask();
-
+        createTask(userInput.value);
+        saveTask();
     }
 })
 
+addTaskBtn.addEventListener("click", () => {
+    if (!(userInput.value == "")) {
+        createTask(userInput.value);
+        saveTask();
+    }
 
-addTaskBtn.addEventListener("click",()=>{
-    createTask();
 })
+function getLsData() {
+    const lsTaskget = JSON.parse(localStorage.getItem("tasks"));
+    lsTaskget.forEach((ls) => {
+        createTask(ls);
+    })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    getLsData()
+
+});
 
 
